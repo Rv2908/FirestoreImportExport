72844e3193b9f6d9dbbc34160de8cec0fdf0e9a2
Ravi Waghmare <RaviW@WinjitTechnologiesPvtLtd.local>
2019-07-10 15:42:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const firestoreService = require("firestore-export-import");
+const serviceAccount = require("./serviceAccount.json");
+var fs = require('fs');
+
+// Initiate Firebase App
+firestoreService.initializeApp(
+  serviceAccount,
+  "https://wholechain.firebaseio.com"
+);
+
+const main = {};
+
+
+// Firebase Import data from firestore database
+firestoreService
+  .backups([
+    "collection 1",
+    "collection 2",
+  ])
+  .then(collections => {
+    fs.writeFile("fileName.json", JSON.stringify(collections), function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    
+  });
